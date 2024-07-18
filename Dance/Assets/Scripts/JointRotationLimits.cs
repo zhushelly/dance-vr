a9d12e465a30074b7e6d9c2e4bca6ae9d0aa4d95
zhushelly <zhixiuzhu@ucsb.edu>
2024-07-17 17:59:55 -0700

    mirror and joint rotations

--- Dance/Assets/Scripts/JointRotationLimits.cs
+++ Dance/Assets/Scripts/JointRotationLimits.cs
@@ -1,24 +1,24 @@
 using RootMotion.FinalIK;
 using UnityEngine;
 
-public class ConfigureRotationLimits : MonoBehaviour
+public class JointRotationLimits : MonoBehaviour
 {
     public VRIK ik;
     
     void Start()
     {
         // Assuming ik.references.leftForearm and ik.references.rightForearm are the elbow joints
-        AddRotationLimit(ik.references.leftForearm, new Vector3(1, 0, 0), 90); // Limit left elbow to 90 degrees on the X axis
-        AddRotationLimit(ik.references.rightForearm, new Vector3(1, 0, 0), 90); // Limit right elbow to 90 degrees on the X axis
+        AddRotationLimit(ik.references.leftForearm, new Vector3(1, 0, 0), 180); // Limit left elbow to 180 degrees on the X axis
+        AddRotationLimit(ik.references.rightForearm, new Vector3(1, 0, 0), 180); // Limit right elbow to 180 degrees on the X axis
 
         // Assuming ik.references.leftHand and ik.references.rightHand are the wrist joints
-        AddRotationLimit(ik.references.leftHand, new Vector3(1, 0, 0), 45); // Limit left wrist to 45 degrees on the X axis
-        AddRotationLimit(ik.references.leftHand, new Vector3(0, 1, 0), 45); // Limit left wrist to 45 degrees on the Y axis
-        AddRotationLimit(ik.references.leftHand, new Vector3(0, 0, 1), 45); // Limit left wrist to 45 degrees on the Z axis
+        AddRotationLimit(ik.references.leftHand, new Vector3(1, 0, 0), 90); // Limit left wrist to 90 degrees on the X axis
+        AddRotationLimit(ik.references.leftHand, new Vector3(0, 1, 0), 90); // Limit left wrist to 90 degrees on the Y axis
+        AddRotationLimit(ik.references.leftHand, new Vector3(0, 0, 1), 90); // Limit left wrist to 90 degrees on the Z axis
 
-        AddRotationLimit(ik.references.rightHand, new Vector3(1, 0, 0), 45); // Limit right wrist to 45 degrees on the X axis
-        AddRotationLimit(ik.references.rightHand, new Vector3(0, 1, 0), 45); // Limit right wrist to 45 degrees on the Y axis
-        AddRotationLimit(ik.references.rightHand, new Vector3(0, 0, 1), 45); // Limit right wrist to 45 degrees on the Z axis
+        AddRotationLimit(ik.references.rightHand, new Vector3(1, 0, 0), 90); // Limit right wrist to 90 degrees on the X axis
+        AddRotationLimit(ik.references.rightHand, new Vector3(0, 1, 0), 90); // Limit right wrist to 90 degrees on the Y axis
+        AddRotationLimit(ik.references.rightHand, new Vector3(0, 0, 1), 90); // Limit right wrist to 90 degrees on the Z axis
     }
 
     void AddRotationLimit(Transform joint, Vector3 axis, float limit)
