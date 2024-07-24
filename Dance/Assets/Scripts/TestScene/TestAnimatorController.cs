071e9f10c1ada59e8a5bbc4eb73baeee7c7c7129
zhushelly <zhixiuzhu@ucsb.edu>
2024-07-24 16:14:36 -0700

    animation switch by pressing key '1' or key '2'

--- Dance/Assets/Scripts/TestScene/TestAnimatorController.cs
+++ Dance/Assets/Scripts/TestScene/TestAnimatorController.cs
@@ -1,32 +1,28 @@
 using UnityEngine;
 
-public class TestAnimatorController : MonoBehaviour
+public class AnimationController : MonoBehaviour
 {
     private Animator animator;
 
     void Start()
     {
         animator = GetComponent<Animator>();
-        if (animator == null)
-        {
-            Debug.LogError("Animator component not found on the game object.");
-        }
     }
 
     void Update()
     {
+        // Switch to BA-Echappe animation when pressing '1'
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            Debug.Log("1 key pressed, switching to Noh-Suriashi animation.");
-            animator.SetTrigger("SwitchToNoh");
-            Debug.Log("Current State: " + animator.GetCurrentAnimatorStateInfo(0).IsName("Noh-Suriashi"));
+            animator.SetBool("SwitchToBA", true);
+            animator.SetBool("SwitchToNoh", false);
         }
 
+        // Switch to Noh-Suriashi animation when pressing '2'
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-            Debug.Log("2 key pressed, switching to BA-Echappe animation.");
-            animator.SetTrigger("SwitchToBA");
-            Debug.Log("Current State: " + animator.GetCurrentAnimatorStateInfo(0).IsName("BA-Echappe"));
+            animator.SetBool("SwitchToNoh", true);
+            animator.SetBool("SwitchToBA", false);
         }
     }
 }
